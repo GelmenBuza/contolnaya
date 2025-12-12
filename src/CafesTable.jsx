efd3c43bf9e22bad8ff75d43a10e602d25d4d9e3
Zabelin Danil <danil.danich.22825@gmail.com>
2025-12-12 14:13:03 +0300

    done

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,1 +1,39 @@
+import FilterCafes from "./FilterCafes.jsx";
+import {useEffect, useState} from "react";
 
+
+export default function CafesTable() {
+    const [cafes, setCafes] = useState([]);
+    const [subways, setSubways] = useState('All');
+
+    useEffect(() => {
+        fetch('/cafes')
+            .then((response) => response.json())
+            .then(data => setCafes(data.cafes || []))
+            .catch(err => console.log(err));
+    }, []);
+    console.log(cafes);
+
+    const filterCafes = subways === 'All' ?
+        cafes
+        :
+        cafes.filter(cafe => cafe.subwayCode.includes(subways));
+
+    return (
+        <div className="cafesTable">
+            <FilterCafes value={subways} onChange={setSubways}></FilterCafes>
+            <ul className='cardsList'>
+                {filterCafes.map((cafe) => (
+                    <li className='card' key={cafe.id}>
+                        <img src={cafe.img || 'https://via.placeholder.com/150'} alt=""/>
+                        <h2>{cafe.name}</h2>
+                        <p>{cafe.desc}</p>
+                        <p>{cafe.address}</p>
+                        <p>{cafe.subwayCode}</p>
+                        <p>{cafe.workTime}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,15 @@
-
+export default function FilterCafes({value, onChange}) {
+    const handleSwap = (e) => {
+        onChange(e.target.value)
+    }
+    return <div className="controls">
+        <select name="subway" id="subway" value={value} onChange={handleSwap}>
+            <option value="All">Все</option>
+            <option value="Arbat">Арбатская</option>
+            <option value="Alexanders Garden">Александровский сад</option>
+            <option value="Moscow">Московская</option>
+            <option value="Culture">Парк Культуры</option>
+            <option value="Theater">Театральная</option>
+        </select>
+    </div>
+}
